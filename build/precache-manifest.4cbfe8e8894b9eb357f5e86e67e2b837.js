de0e9bcd7f07cad819d7e8daaf6969aff87d5686
vdmemory <vdmemory2@gmail.com>
2019-11-22 13:13:41 +0200

    Added build directory

---
+++ build/precache-manifest.4cbfe8e8894b9eb357f5e86e67e2b837.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc135cb35afffd91e6f07fce8a584499",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9f0b26098eeaf68b69c",
+    "url": "/static/css/2.dc1eab0f.chunk.css"
+  },
+  {
+    "revision": "6cc8d9bf0f08a5e9d712",
+    "url": "/static/css/main.960e3038.chunk.css"
+  },
+  {
+    "revision": "d9f0b26098eeaf68b69c",
+    "url": "/static/js/2.efef1870.chunk.js"
+  },
+  {
+    "revision": "6cc8d9bf0f08a5e9d712",
+    "url": "/static/js/main.27f29580.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d89dad86bca83abfb643db14b634069e",
+    "url": "/static/media/avatarDefault.d89dad86.png"
+  },
+  {
+    "revision": "d1d4c9ba47c2ed0211a8d8d477345120",
+    "url": "/static/media/error.d1d4c9ba.png"
+  }
+]);
